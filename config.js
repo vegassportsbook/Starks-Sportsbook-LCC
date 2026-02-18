c34dd88622dc66768b1c5bf8f8ef8b44107114b5
Jason Starks <your-real-github-email@example.com>
2026-02-18 09:17:47 -0600

    fix config key

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-  API_BASE: "https://script.google.com/macros/s/AKfycbyOBFTIyLttov0efY0b-qAYRuZbFiLFTmp8JxaNXZBx/exec"
+  GAS_WEBAPP_URL: "https://script.google.com/macros/s/AKfycbyOBFTIyLttov0efY0b-qAYRuZbFiLFTmp8JxaNXZBx/exec"
 };
 
 export default CONFIG;
